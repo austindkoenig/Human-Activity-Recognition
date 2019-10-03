052f4259f46e9d4ec246d84e6a42e2071e153ea8
Austin Koenig <austindkoenig@gmail.com>
2019-10-03 12:13:10 -0700

    Creates table examining sequences.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Human-Activity-Recognition
+# <center>Human Activity Recognition</center>
+
+<center>Austin Koenig</center>
+
+## Issues and Notes
+
+| Activity Name | # Sequences | % Sequences | Min Seq Length | Max Seq Length |
+| --- | --- | --- | --- | --- |
+| Brush_teeth | 12 | 0.014303 | 844 | 3199 |
+| Climb_stairs | 102 | 0.121573 | 166 | 3199 |
+| Comb_hair | 31 | 0.036949 | 166 | 3199 |
+| Descend_stairs | 42 | 0.05006 | 156 | 3199 |
+| Drink_glass | 100 | 0.11919 | 156 | 3199 |
+| Eat_meat | 5 | 0.005959 | 156 | 9318 |
+| Eat_soup | 3 | 0.003576 | 156 | 9318 |
+| Getup_bed | 101 | 0.120381 | 156 | 9318 |
+| Liedown_bed | 28 | 0.033373 | 156 | 9318 |
+| Pour_water | 100 | 0.11919 | 156 | 9318 |
+| Sitdown_chair | 100 | 0.11919 | 125 | 9318 |
+| Standup_chair | 102 | 0.121573 | 125 | 9318 |
+| Use_telephone | 13 | 0.015495 | 125 | 9318 |
+| Walk | 100 | 0.11919 | 125 | 9318 | 
+
+
+## Acknowledgments
+
+These data come from [this repository](https://archive.ics.uci.edu/ml/datasets/Dataset+for+ADL+Recognition+with+Wrist-worn+Accelerometer).
--- koenig-har.py
+++ koenig-har.py
@@ -11,24 +11,61 @@ import os
 #from keras import models
 #from keras import layers
 #from keras import optimizers
+#from keras.preprocessing import sequence
 
 class HARSystem(object):
+    '''
+    Human Activity Recognition (HAR) System class.
+    '''
     def __init__(self, data_fp = './data/'):
         self.data_fp = data_fp
+        self.scaler = None
+
+        self.labels = None
         self.raw_data = None
         self.data = None
+
         self.train = None
         self.val = None
         self.test = None
     
-    def preprocess(self):
-        label_bank = os.listdir(self.data_fp)
-        for i in range(len(label_bank)):
-            root = os.path.join(self.data_fp, label_bank[i])
+    def preprocess_data(self):
+        '''
+        Prepares the data for the activity recognizer.
+
+        Returns
+        -------
+        trainX : numpy.array
+            Training sequences
+        '''
+
+        rawX = []
+        rawY = []
+
+        self.labels = os.listdir(self.data_fp)
+
+        # Extract data from text files
+        for i in range(len(self.labels)):
+            root = os.path.join(self.data_fp, self.labels[i])
             files = os.listdir(root)
+            class_samples = 0
+
             for f in files:
                 x = np.genfromtxt(os.path.join(root, f), delimiter = ' ')
-                y = label_bank[i]
+                rawX.append(x)
+                rawY.append(i)
+                class_samples += 1
+            
+            min_length = min([len(x) for x in rawX])
+            max_length = max([len(x) for x in rawX])
+            print(f'| {self.labels[i]} | {class_samples} | {round(class_samples / 839, 6)} | {min_length} | {max_length} |')
+        print()
+        
+        print(len(rawX))
+        #rawX = np.reshape(np.dstack(rawX), (len(rawX), 3, -1))
+        #rawY = np.array(rawY)
+        #print(rawX.shape, rawY.shape)
+        #self.raw_data = (rawX, rawY)
 
     def build_model(self):
         pass
@@ -40,7 +77,7 @@ class HARSystem(object):
 
 if __name__ == "__main__":
     rec = HARSystem()
-    rec.preprocess()
+    rec.preprocess_data()
 
 
 
